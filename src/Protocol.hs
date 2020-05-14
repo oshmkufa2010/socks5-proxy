628954c5e132c39955355520ee47ad96227daca0
cailin <cailinunix@gmail.com>
2020-05-14 17:01:50 +0800

    Add domainname atyp

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Protocol(
   buildSocks5Connection,
   runSocketEff,
@@ -5,6 +7,7 @@ module Protocol(
 
 import qualified Data.ByteString as BS
 import qualified Control.Exception as E
+import qualified Data.ByteString.Char8 as C
 import Data.ByteString (ByteString(..))
 import Network.Socket
 import Data.Word (Word8(..))
@@ -26,8 +29,8 @@ newConnection host port = do
       connect sock $ addrAddress addr
       return sock
 
-toIPv4 :: ByteString -> String
-toIPv4 addr = concat $ intersperse "." $ fmap (show . fromIntegral) $ BS.unpack addr
+toIP :: ByteString -> String
+toIP addr = concat $ intersperse "." $ fmap (show . fromIntegral) $ BS.unpack addr
 
 toPort :: ByteString -> String
 toPort port = show $ foldl (\b a -> b * 256 + a) 0 $ fmap fromIntegral $ BS.unpack port
@@ -41,31 +44,33 @@ recvVerAndMethod = do
   when (0x00 `notElem` (BS.unpack methods)) $ throwSocketError "no method supported"
   return (ver, 0x00)
 
-recvTargetAddress :: SocketEff (Word8, Word8, ByteString, ByteString)
+recvTargetAddress :: SocketEff (Word8, Word8, String, String, ByteString)
 recvTargetAddress = do
   bs <- recvNBytes 4
   let [ver, cmd, _, atyp] = BS.unpack bs
-  addrLen <- do
+  (addr, encodedAddr) <- do
     case atyp of
-      0x01 -> return 4
-      0x03 -> throwSocketError "unsupported atyp: domain"
-      0x04 -> return 16
+      0x01 -> fmap (\ip -> (toIP ip, ip)) (recvNBytes 4)
+      0x04 -> fmap (\ip -> (toIP ip, ip)) (recvNBytes 16)
+      0x03 -> do
+        len <- recvNBytes 1
+        domain <- recvNBytes (fromIntegral (BS.head len))
+        return (C.unpack domain, BS.append len domain)
       _ -> throwSocketError "unsupported atyp"
-  addr <- recvNBytes addrLen
   port <- recvNBytes 2 
-  return (cmd, atyp, addr, port)
+  return (cmd, atyp, addr, toPort port, BS.append encodedAddr port)
 
 buildSocks5Connection :: SocketEff Socket
 buildSocks5Connection = do
   (ver, method) <- recvVerAndMethod
   sendAllBytes (BS.pack [ver, method])
-  (cmd, atyp, addr, port) <- recvTargetAddress
+  (cmd, atyp, addr, port, encodedAddrAndPort) <- recvTargetAddress
   when (cmd /= 0x01) $ throwSocketError ("unsupported cmd: " ++ (show cmd))
-  remoteSocket <- liftIO (E.try $ newConnection (toIPv4 addr) (toPort port) :: IO (Either E.SomeException Socket))
-  case remoteSocket of
+  mSocket :: Either E.SomeException Socket <- liftIO $ E.try $ newConnection addr port
+  case mSocket of
     Left e -> do
-      sendAllBytes $ ver `BS.cons` 0x01 `BS.cons` 0x00 `BS.cons` atyp `BS.cons` (addr `BS.append` port)
+      sendAllBytes $ ver `BS.cons` 0x01 `BS.cons` 0x00 `BS.cons` atyp `BS.cons` encodedAddrAndPort
       throwSocketError (show e)
     Right socket -> do
-      sendAllBytes $ ver `BS.cons` 0x00 `BS.cons` 0x00 `BS.cons` atyp `BS.cons` (addr `BS.append` port)
+      sendAllBytes $ ver `BS.cons` 0x00 `BS.cons` 0x00 `BS.cons` atyp `BS.cons` encodedAddrAndPort
       return socket
