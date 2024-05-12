e7a54ff9ff6d3c04646bff2d0b12723b5f0bb641
oshmkufa2010 <cailinunix@gmail.com>
2024-05-12 17:14:14 +0800

    Refactor

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -1,28 +1,60 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Protocol(
-  buildSocks5Connection,
-  runSocketEff,
-) where
+module Protocol
+  ( buildSocks5Connection,
+  )
+where
 
-import qualified Data.ByteString as BS
 import qualified Control.Exception as E
-import qualified Data.ByteString.Char8 as C
-import Data.ByteString (ByteString(..))
-import Network.Socket
-import Data.Word (Word8(..))
-import Control.Monad.IO.Class
-import Effect
 import Control.Monad (when)
-import Data.List (intersperse)
+import Control.Monad.Except
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.ByteString (ByteString (..))
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C
+import Data.Word (Word8 (..))
+import Network.Socket
+import Network.Socket.ByteString (recv, sendAll)
+import Data.List (intercalate)
 
-newConnection:: String -> String -> IO Socket
+type SocketEff m = (MonadReader Socket m, MonadState ByteString m, MonadIO m, MonadError String m)
+
+recvBytesWithoutBuffer :: SocketEff m => m ByteString
+recvBytesWithoutBuffer = do
+  socket <- ask
+  bs <- liftIO (recv socket 4096)
+  when (BS.length bs == 0) $ throwError $ show socket ++ ": the peer has closed its half side of the connection."
+  return bs
+
+recvNBytes :: SocketEff m => Int -> m ByteString
+recvNBytes size
+  | size <= 0 = return BS.empty
+  | otherwise = do
+    buffer <- get
+    if BS.length buffer >= size
+      then do
+        put (BS.drop size buffer)
+        return (BS.take size buffer)
+      else do
+        bs <- recvBytesWithoutBuffer
+        put (BS.append buffer bs)
+        recvNBytes size
+
+sendAllBytes :: SocketEff m => ByteString -> m ()
+sendAllBytes bs = do
+  socket <- ask
+  liftIO (sendAll socket bs)
+
+newConnection :: String -> String -> IO Socket
 newConnection host port = do
   addr <- resolve
   open addr
   where
     resolve = do
-      let hints = defaultHints { addrSocketType = Stream }
+      let hints = defaultHints {addrSocketType = Stream}
       head <$> getAddrInfo (Just hints) (Just host) (Just port)
     open addr = do
       sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
@@ -30,21 +62,21 @@ newConnection host port = do
       return sock
 
 toIP :: ByteString -> String
-toIP addr = concat $ intersperse "." $ fmap (show . fromIntegral) $ BS.unpack addr
+toIP addr = intercalate "." (show . fromIntegral <$> BS.unpack addr)
 
 toPort :: ByteString -> String
-toPort port = show $ foldl (\b a -> b * 256 + a) 0 $ fmap fromIntegral $ BS.unpack port
+toPort port = show $ foldl (\b a -> b * 256 + a) 0 $ fromIntegral <$> BS.unpack port
 
-recvVerAndMethod :: SocketEff (Word8, Word8)
+recvVerAndMethod :: SocketEff m => m (Word8, Word8)
 recvVerAndMethod = do
   result <- recvNBytes 2
   let [ver, methodn] = BS.unpack result
-  when (ver /= 0x05) $ throwSocketError $ userError ("unsupported ver: " ++ (show ver))
+  when (ver /= 0x05) $ throwError ("unsupported ver: " ++ show ver)
   methods <- recvNBytes (fromIntegral methodn)
-  when (0x00 `notElem` (BS.unpack methods)) $ throwSocketError $ userError "no method supported"
+  when (0x00 `notElem` BS.unpack methods) $ throwError "no method supported"
   return (ver, 0x00)
 
-recvTargetAddress :: SocketEff (Word8, Word8, String, String, ByteString)
+recvTargetAddress :: SocketEff m => m (Word8, Word8, String, String, ByteString)
 recvTargetAddress = do
   bs <- recvNBytes 4
   let [ver, cmd, _, atyp] = BS.unpack bs
@@ -56,21 +88,21 @@ recvTargetAddress = do
         len <- recvNBytes 1
         domain <- recvNBytes (fromIntegral (BS.head len))
         return (C.unpack domain, BS.append len domain)
-      _ -> throwSocketError $ userError "unsupported atyp"
+      _ -> throwError "unsupported atyp"
   port <- recvNBytes 2
   return (cmd, atyp, addr, toPort port, BS.append encodedAddr port)
 
-buildSocks5Connection :: SocketEff Socket
+buildSocks5Connection :: SocketEff m => m Socket
 buildSocks5Connection = do
   (ver, method) <- recvVerAndMethod
   sendAllBytes (BS.pack [ver, method])
   (cmd, atyp, addr, port, encodedAddrAndPort) <- recvTargetAddress
-  when (cmd /= 0x01) $ throwSocketError $ userError ("unsupported cmd: " ++ (show cmd))
+  when (cmd /= 0x01) $ throwError ("unsupported cmd: " ++ show cmd)
   mSocket :: Either E.SomeException Socket <- liftIO $ E.try $ newConnection addr port
   case mSocket of
     Left e -> do
       sendAllBytes $ ver `BS.cons` 0x01 `BS.cons` 0x00 `BS.cons` atyp `BS.cons` encodedAddrAndPort
-      throwSocketError $ userError (show e)
+      throwError $ "connection " ++ addr ++ " " ++ port ++ " error: " ++ show e
     Right socket -> do
       sendAllBytes $ ver `BS.cons` 0x00 `BS.cons` 0x00 `BS.cons` atyp `BS.cons` encodedAddrAndPort
       return socket
--- src/Server.hs
+++ src/Server.hs
@@ -1,18 +1,23 @@
 module Server
-  (
-    runTCPServer,
-    socks5Server
-  ) where
+  ( runTCPServer,
+    socks5Server,
+  )
+where
 
-import Network.Socket
-import Network.Socket.ByteString (recv, sendAll)
-import qualified Control.Exception as E
-import Control.Monad (void, forever)
 import Control.Concurrent
 import Control.Concurrent.Async
-import Protocol (buildSocks5Connection, runSocketEff)
+import qualified Control.Exception as E
+import Control.Monad (forever, void, when)
+import Control.Monad.Reader
+import Control.Monad.State
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import Network.Socket
+import Network.Socket.ByteString (recv, sendAll)
+import Protocol (buildSocks5Connection)
 import System.IO.Error (userError)
+import Control.Monad.Except (ExceptT, runExceptT)
+import Control.Exception (bracket)
 
 runTCPServer :: Maybe HostName -> ServiceName -> (Socket -> IO a) -> IO a
 runTCPServer mhost port server = withSocketsDo $ do
@@ -20,15 +25,16 @@ runTCPServer mhost port server = withSocketsDo $ do
   E.bracket (open addr) close loop
   where
     resolve = do
-      let hints = defaultHints {
-        addrFlags = [AI_PASSIVE]
-        , addrSocketType = Stream
-      }
+      let hints =
+            defaultHints
+              { addrFlags = [AI_PASSIVE],
+                addrSocketType = Stream
+              }
       head <$> getAddrInfo (Just hints) mhost (Just port)
     open addr = do
       sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
       setSocketOption sock ReuseAddr 1
-      withFdSocket sock $ setCloseOnExecIfNeeded
+      withFdSocket sock setCloseOnExecIfNeeded
       bind sock $ addrAddress addr
       listen sock 1024
       return sock
@@ -36,21 +42,24 @@ runTCPServer mhost port server = withSocketsDo $ do
       (conn, _peer) <- accept sock
       void $ forkFinally (server conn) (const $ gracefulClose conn 5000)
 
+runSocketEff :: ReaderT Socket (StateT ByteString (ExceptT String IO)) a -> Socket -> IO a
+runSocketEff eff socket = do
+  x <- runExceptT $ evalStateT (runReaderT eff socket) BS.empty
+  case x of
+    Left e -> do
+      putStrLn $ "Error: " ++ e
+      ioError $ userError e
+    Right a -> pure a
+
+
 socks5Server :: Socket -> IO ()
 socks5Server clientSocket = do
-  socket <- runSocketEff buildSocks5Connection clientSocket
-  a <- async (forward clientSocket socket)
-  b <- async (forward socket clientSocket)
-  waitCatch a
-  waitCatch b
-  gracefulClose socket 5000
+  bracket (runSocketEff buildSocks5Connection clientSocket) (`gracefulClose` 5000) $ \socket ->
+    concurrently_ (forward clientSocket socket) (forward socket clientSocket)
   where
     forward :: Socket -> Socket -> IO ()
     forward from to = do
       bs <- recv from 4096
-      if BS.length bs > 0
-      then do
+      when (BS.length bs > 0) $ do
         sendAll to bs
         forward from to
-      else
-        return ()
